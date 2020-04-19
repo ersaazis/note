d0b199a69130eea87de65df5c2ab1ebfe33fca44
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-19 15:00:44 +0700

    Update docker-deb10.bash

--- docker-deb10.bash
+++ docker-deb10.bash
@@ -9,10 +9,9 @@ apt-get install \
     software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
-reboot
