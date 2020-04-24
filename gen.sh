a04a34870c2565984edd05a18747153c868ff6b4
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-24 09:31:06 +0700

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,6 +7,6 @@ echo Port VNC :
 read port
 
 echo ini urlnya $url
-docker container create -e URL='$url' -p 5900:$port --name $name --cpus=1 --memory="450m" --memory-swap="1g" gbyt:1
+docker container create -e URL='$url' -p 5900:$port --name $name --cpus=1 gbyt:1
 docker container start $name
 docker exec -i -t $name /bin/bash
