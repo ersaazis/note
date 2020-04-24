7780c0b6ea7a2146b01c0410e053e05d2f304aa7
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-24 16:33:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nice -n 13 Xvfb -retro -render -screen 0 600x650x16 :99 &
-export {http,https,ftp}_proxy=${PROXY}
+if [ "$PROXY" != "" ]; then export {http,https,ftp}_proxy=${PROXY} ;fi
 rm /root/.mozilla -r
 cp -r /root/data /root/.mozilla
 nice -n 13 x11vnc -display :99 &
