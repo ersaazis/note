3168ac17d89f831a498a3a84bf024144633219f2
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-24 09:56:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@ nice -n 13 Xvfb -retro -render -screen 0 600x650x16 :99 &
 rm /root/.mozilla -r
 cp -r /root/data /root/.mozilla
 nice -n 13 x11vnc -display :99 &
-nice -n 13 firefox-esr --display :99 -p 1
-nice -n 13 firefox-esr --display :99 -p 1&
+#nice -n 13 firefox-esr --display :99 -p 1
+nice -n 13 firefox-esr ifconfig.me --display :99 -p 1&
 sleep 30 && \
-nice -n 13 firefox-esr --new-tab "https://www.youtube.com/watch?v=owjfqbVHpgw&list=PLzy1GtZA-b9jd6dQUvilLgmK2WhplAq4Z&index=1&t=0s" --display :99 && \
-nice -n 13 firefox-esr --new-tab "https://www.youtube.com/watch?v=owjfqbVHpgw&list=PLzy1GtZA-b9jd6dQUvilLgmK2WhplAq4Z&index=1&t=0s" --display :99 && \
-nice -n 13 firefox-esr --new-tab "https://www.youtube.com/watch?v=owjfqbVHpgw&list=PLzy1GtZA-b9jd6dQUvilLgmK2WhplAq4Z&index=1&t=0s" --display :99 && \
-nice -n 13 firefox-esr --new-tab "https://www.youtube.com/watch?v=owjfqbVHpgw&list=PLzy1GtZA-b9jd6dQUvilLgmK2WhplAq4Z&index=1&t=0s" --display :99 && \
-nice -n 13 firefox-esr --new-tab "https://www.youtube.com/watch?v=owjfqbVHpgw&list=PLzy1GtZA-b9jd6dQUvilLgmK2WhplAq4Z&index=1&t=0s" --display :99 &
+nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
+nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
+nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
+nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
+nice -n 13 firefox-esr --new-tab ${URL} --display :99 &
+export {http,https,ftp}_proxy=${PROXY}
 tail -f /dev/null
