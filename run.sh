e61270e9a37aa1bcac9348c0ebba8a92ae0dfc88
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-27 18:58:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 nice -n 13 Xvfb -retro -render -screen 0 600x650x16 :99 &
+nice -n 13 x11vnc -display :99 &
 if [ "$PROXY" != "" ]; then export {http,https,ftp}_proxy=${PROXY} ;fi
+
+
+while :
+do
 rm /root/.mozilla -r
 cp -r /root/data /root/.mozilla
-nice -n 13 x11vnc -display :99 &
-#nice -n 13 firefox-esr --display :99 -p 1
 nice -n 13 firefox-esr ifconfig.me --display :99 -p 1&
 sleep 30 && \
 nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
@@ -12,4 +15,6 @@ nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
 nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
 nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
 nice -n 13 firefox-esr --new-tab ${URL} --display :99 &
-tail -f /dev/null
+sleep 6000
+pkill firefox-esr
+done
