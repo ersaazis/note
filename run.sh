120bff8e598fe5f14c7fa995ee0d6bdc1d432ae7
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-28 11:54:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,27 @@ nice -n 13 Xvfb -retro -render -screen 0 600x650x16 :99 &
 nice -n 13 x11vnc -display :99 &
 if [ "$PROXY" != "" ]; then export {http,https,ftp}_proxy=${PROXY} ;fi
 
+rm /root/.mozilla -r
+cp -r /root/data /root/.mozilla
+
+cd /root
+python -m SimpleHTTPServer &
 
 while :
 do
-rm /root/.mozilla -r
-cp -r /root/data /root/.mozilla
+pkill firefox-esr
+
+rm /root/.mozilla/firefox/4b2bgj9y.1/session* -r
+rm /root/.mozilla/firefox/4b2bgj9y.1/places.sqlite
+wget https://youtube.bisnisglobal.com/url/$ID.js -O /root/my.js
+
 nice -n 13 firefox-esr ifconfig.me --display :99 -p 1&
 sleep 30 && \
-nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
-nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
-nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
-nice -n 13 firefox-esr --new-tab ${URL} --display :99 && \
-nice -n 13 firefox-esr --new-tab ${URL} --display :99 &
-sleep 6000
+nice -n 13 firefox-esr --new-tab https://youtube.bisnisglobal.com/ref/$ID --display :99 && \
+nice -n 13 firefox-esr --new-tab https://youtube.bisnisglobal.com/ref/$ID --display :99 && \
+nice -n 13 firefox-esr --new-tab https://youtube.bisnisglobal.com/ref/$ID --display :99 && \
+nice -n 13 firefox-esr --new-tab https://youtube.bisnisglobal.com/ref/$ID --display :99 && \
+nice -n 13 firefox-esr --new-tab https://youtube.bisnisglobal.com/ref/$ID --display :99 &
+sleep 600
 pkill firefox-esr
 done
