8c0ccd93d85c4655cb823574c252e84702c27ba6
Ersa Azis Mansyur <37845294+ersaazis@users.noreply.github.com>
2020-04-28 12:04:40 +0700

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -8,6 +8,6 @@ read port
 echo Proxy :
 read proxy
 
-docker container create -e id=$ID -e PROXY=$proxy -p $port:5900 --name $name --cpus=0.9 gbyt:1
+docker container create -e ID=$id -e PROXY=$proxy -p $port:5900 --name $name --cpus=0.9 gbyt:1
 docker container start $name
 docker exec -i -t $name /bin/bash
